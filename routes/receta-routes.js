6377dca87d1ff01651f8330b3d0147b41f84ddf3
LaraMigueltorena <migueltorenalara@gmail.com>
2025-07-01 00:30:27 -0300

    vista home

--- routes/receta-routes.js
+++ routes/receta-routes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const recetaController = require('../controllers/receta-controller');
 
+
+router.get('/ultimas', recetaController.getUltimasRecetas);
+router.get('/:id', recetaController.getRecetaPorId);
 router.get('/', recetaController.getAllRecetas);
 router.post('/', recetaController.createReceta);
 router.put('/:id', recetaController.updateReceta);
 router.delete('/:id', recetaController.deleteReceta);
+
 
 
 module.exports = router;
